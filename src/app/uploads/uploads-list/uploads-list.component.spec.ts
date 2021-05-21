fb554ef70564f8ee9fc7d9f184c92398d26f46e7
Gabriel Nicutari <gabriel_nicutari@yahoo.ro>
2021-05-21 20:46:33 +0200

    Generated Upload Comonents

---
+++ src/app/uploads/uploads-list/uploads-list.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UploadsListComponent } from './uploads-list.component';
+
+describe('UploadsListComponent', () => {
+  let component: UploadsListComponent;
+  let fixture: ComponentFixture<UploadsListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UploadsListComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UploadsListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
